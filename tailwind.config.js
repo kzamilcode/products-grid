dda55a835d257a820543ecdf32cc642d51e2f22f
Antu-code <jibonoshomapto3@gmail.com>
2022-09-14 08:15:30 +0600

    new-update

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+ 
+  theme: {
+
+ 
+    fontFamily:{
+        "poppins" : "'Poppins', sans-serif"
+    },
+    extend: {
+   
+ 
+ 
+      boxShadow: {
+      
+       
+      },
+     colors:{
+        'black': '#000000',
+        'pink' : '#E56C7B',
+        'yellow':'#FECF22',
+        'red' : '#DD2F90',
+        'creaam' : '#D1C2DC',
+        'Eyeliner' : '#B0DEDC',
+        'Product' :'#E0C0A7',
+        'blue': '#0316BD',
+        'creaam-new' : '#999999',
+        'nGray': '#767676'
+
+
+       
+        
+        
+     },
+     animation :{
+      
+    },
+     keyframes :{ },
+     transitionDuration: {
+     
+      
+    },
+      width: {
+     
+      },
+      height: {
+       
+      },
+      inset: {
+        
+      }
+    },
+  },
+  variants: {
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
